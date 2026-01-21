42ad5dbdb3231613cba2aa0b293c0af82eccb95e
GreateLordAvocado <sinner.halo@yandex.ru>
2026-01-21 21:01:17 +0700

    refactor: использован Lombok для внедрения зависимостей в EventsController

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/controller/EventsController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/controller/EventsController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.kafka.telemetry.collector.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.kafka.telemetry.collector.dto.hub.HubEvent;
@@ -11,31 +12,24 @@ import ru.yandex.practicum.kafka.telemetry.collector.mapper.SensorEventAvroMappe
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventsController {
 
     private final SensorEventAvroMapper sensorMapper;
     private final HubEventAvroMapper hubMapper;
     private final TelemetryKafkaProducer kafkaProducer;
 
-    public EventsController(SensorEventAvroMapper sensorMapper,
-                            HubEventAvroMapper hubMapper,
-                            TelemetryKafkaProducer kafkaProducer) {
-        this.sensorMapper = sensorMapper;
-        this.hubMapper = hubMapper;
-        this.kafkaProducer = kafkaProducer;
-    }
-
     @PostMapping("/sensors")
     public ResponseEntity<Void> collectSensorEvent(@Valid @RequestBody SensorEvent event) {
         var avro = sensorMapper.toAvro(event);
         kafkaProducer.sendSensor(avro);
-        return ResponseEntity.ok().build(); // 200 OK по спецификации
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/hubs")
     public ResponseEntity<Void> collectHubEvent(@Valid @RequestBody HubEvent event) {
         var avro = hubMapper.toAvro(event);
         kafkaProducer.sendHub(avro);
-        return ResponseEntity.ok().build(); // 200 OK по спецификации
+        return ResponseEntity.ok().build();
     }
 }
